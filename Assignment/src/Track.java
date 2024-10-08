9050e6b431a88a53be1c4eea35269d5cbcbd7d58
RyzyO <89904489+RyzyO@users.noreply.github.com>
2024-10-09 08:48:26 +1100

    Paramatised Constructor for Class Track

--- Assignment/src/Track.java
+++ Assignment/src/Track.java
@@ -1,3 +1,12 @@
 public class Track {
-    
+    String TrackName;
+    String Artist; 
+    Float Duration;
+
+    public Track(String TrackName, String Artist, Float Duration) {
+        this.TrackName = TrackName;
+        this.Artist = Artist;
+        this.Duration = Duration;
+    }
+
 }
