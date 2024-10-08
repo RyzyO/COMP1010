a58cf6ad9311c86758417177dbb19b46482b1850
RyzyO <89904489+RyzyO@users.noreply.github.com>
2024-10-09 08:56:46 +1100

    WARNING THIS DOES NOT WORK

--- Assignment/src/Client.java
+++ Assignment/src/Client.java
@@ -1,5 +1,13 @@
-public class App {
+public class Client {
+
+    
+
     public static void main(String[] args) throws Exception {
         System.out.println("COMP1010 Major Assignment");
+
+        Track Never = new Track("Never Gonna Give You Up", "Risk Astley", 3.30);
+
+        println(TrackDetails(Never));
+
     }
 }
--- Assignment/src/Track.java
+++ Assignment/src/Track.java
@@ -1,12 +1,18 @@
 public class Track {
     String TrackName;
     String Artist; 
-    Float Duration;
+    Double Duration;
 
-    public Track(String TrackName, String Artist, Float Duration) {
+    public Track(String TrackName, String Artist, Double Duration) {
         this.TrackName = TrackName;
         this.Artist = Artist;
         this.Duration = Duration;
+    }
+
+    public String TrackDetails() {
+        System.out.println(TrackName);
+        System.out.println(Artist);
+        System.out.println(Duration);
     }
 
 }
