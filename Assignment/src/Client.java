a219a9f2529b56986817aa3bc1dd288a2c2631d7
Jezza184 <jeremy.vella@students.mq.edu.au>
2024-10-12 16:12:22 +1100

    Updated Client.Java and Track.Java
    Should be the updated files. Lmk if they arent. Please check if this is all correct

--- Assignment/src/Client.java
+++ Assignment/src/Client.java
@@ -1,17 +1,19 @@
-
-
 public class Client {
-
-    
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("COMP1010 Major Assignment");
-    
-        Track Never = new Track("Never Gonna Give You Up", "Risk Astley", 3.31, 1);
-        Track Good = new Track("Good Morning", "Kanye West", 3.15, 2);
-        Track currentTrack = new Track();
+        // Create some tracks
+        Track Track1 = new Track("Never Gonna Give You Up", "Risk Astley", 211, 1, "Pop");
+        Track Track2 = new Track("Good Morning", "Kanye West", 195, 2, "Rap");
+        Track Track3 = new Track("Learn To Fly", "Foo Fighters", 2235, 3, "Rock");
         
-        currentTrack.trackDetails();
+
+
+        System.out.println(Track1.getTrackInfo());
+        System.out.println(Track2.getTrackInfo());
+        System.out.println(Track3.getTrackInfo());
+        //Track currentTrack = new Track();
+        
+        //currentTrack.trackDetails();
         
 
     }
