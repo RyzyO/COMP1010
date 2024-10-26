57233423f6cdc65652dd3e6e19a7840f5f159867
RyzyO <89904489+RyzyO@users.noreply.github.com>
2024-10-27 09:31:41 +1100

    tests

--- Assignment/src/Tester.java
+++ Assignment/src/Tester.java
@@ -2,8 +2,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class Tester {
-    @test
-    
+
+    int currentTrackIndex = 1;
+    @Test
+    public void testCurrentTrackIndex() {
+        assertFalse("Invalid Track Index", currentTrackIndex < 0);
+    }
 }
